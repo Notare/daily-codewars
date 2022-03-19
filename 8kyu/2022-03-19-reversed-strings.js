1a447f298045c26a0de9c27bc7bc0d3564c08839
Renato Sousa <renatonotare@gmail.com>
2022-03-19 13:05:20 -0300

    Add "Reversed Strings"

---
+++ 8kyu/2022-03-19-reversed-strings.js
@@ -0,0 +1,9 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// Viewed solution
+function solution(str) {
+  return str.split('').reverse().join('');
+}
