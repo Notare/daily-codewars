a85c38eac9b46228cd6741edf5a18e5261417b19
Renato Sousa <renatonotare@gmail.com>
2022-05-17 11:31:35 -0300

    Add "Third Angle of a Triangle"

---
+++ 8kyu/2022-05-17-third-angle-of-a-triangle.js
@@ -0,0 +1,9 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+const otherAngle = (a, b) => 180 - (a + b);
