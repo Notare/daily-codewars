9c295a5f6649ea8b4510a06f1c91f2f55dfd4d94
Renato Sousa <renatonotare@gmail.com>
2022-07-31 13:02:44 -0300

    Add "Vowel remover"

---
+++ 8kyu/2022-07-31-vowel-remover.js
@@ -0,0 +1,14 @@
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+// Examples
+
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+
+//     don't worry about uppercase vowels
+//     y is not considered a vowel for this kata
+
+function shortcut(string) {
+    return string.split('').filter(letter => !'aeiou'.includes(letter)).join('');
+}
