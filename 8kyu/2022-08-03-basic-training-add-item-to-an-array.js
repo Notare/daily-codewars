349ca1ed55f1ca177dbb30465500364f589ba53d
Renato Sousa <renatonotare@gmail.com>
2022-08-03 10:00:54 -0300

    Add "Basic Training: Add item to an Array"

---
+++ 8kyu/2022-08-03-basic-training-add-item-to-an-array.js
@@ -0,0 +1,8 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+websites.push('codewars');
