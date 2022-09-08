55fd571d28191cd5d6402164a6446ab8a0b1012c
Renato Sousa <renatonotare@gmail.com>
2022-09-08 18:25:53 -0300

    Add "Define a card suit"

---
+++ 8kyu/2022-09-08-define-a-card-suit.js
@@ -0,0 +1,20 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+    if (card.includes('♣')) {
+        return 'clubs';
+    } else if (card.includes('♦')) {
+        return 'diamonds';
+    } else if (card.includes('♥')) {
+        return 'hearts';
+    } else {
+        return 'spades';
+    }
+}
