3e1be4765ab3781a9005909a2afcd30db1f50d99
Renato Sousa <renatonotare@gmail.com>
2022-10-22 13:37:37 -0300

    Add "Swap Values"

---
+++ 8kyu/2022-10-22-swap-values.js
@@ -0,0 +1,10 @@
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+function swapValues() {
+    var args = Array.prototype.slice.call(arguments)[0];
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}
