dd56219c2e3cc32e379303c89d77e1b8d305efbe
Renato Sousa <renatonotare@gmail.com>
2022-12-06 13:02:31 -0300

    Revert "Add "Generate user links""
    This reverts commit dd928f7755e70be082ffd6800c77ae4da1193953.

--- 8kyu/2022-12-05-8kyu-generate-user-links.js
+++
@@ -1,9 +0,0 @@
-// Your task is to create userlinks for the url, you will be given a username and must return a valid link.
-// Example
-
-// generate_link('matt c')
-// http://www.codewars.com/users/matt%20c
-
-function generateLink(user) {
-    return `http://www.codewars.com/users/${encodeURIComponent(user)}`;
-}
