1ed3f12c9ec63ef874e93320920be03e6dd6bcdf
Renato Sousa <renatonotare@gmail.com>
2023-01-07 10:12:06 -0300

    Add "Fizz Buzz"

---
+++ 7kyu/2023-01-07-fizz-buzz.js
@@ -0,0 +1,31 @@
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+//     If the value is a multiple of 3: use the value "Fizz" instead
+//     If the value is a multiple of 5: use the value "Buzz" instead
+//     If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+function fizzbuzz(n) {
+  let arr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+}
