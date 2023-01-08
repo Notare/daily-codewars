debf0f498e483c46f61a712fd75aa550d1a66e23
Renato Sousa <renatonotare@gmail.com>
2023-01-08 11:47:44 -0300

    Add "The Coupon Code"

---
+++ 7kyu/2023-01-08-the-coupon-code.js
@@ -0,0 +1,18 @@
+// Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+// Task
+
+// Your mission:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+// Examples:
+
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  return (
+    enteredCode === correctCode &&
+    new Date(currentDate) <= new Date(expirationDate)
+  );
+}
