84a82ba2cb8060fd5649b9a017bfbbff532ef239
Renato Sousa <renatonotare@gmail.com>
2023-01-14 10:28:38 -0300

    Add "Check the exam"

---
+++ 7kyu/2023-01-14-check-the-exam.js
@@ -0,0 +1,32 @@
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+// If the score < 0, return 0.
+
+// For example:
+
+// checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+// checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+// checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+// checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+function checkExam(correctAnswers, studentAnswers) {
+  let score = 0;
+
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (studentAnswers[i] === correctAnswers[i]) {
+      score += 4;
+    } else if (studentAnswers[i] === "") {
+      score += 0;
+    } else {
+      score -= 1;
+    }
+  }
+
+  if (score < 0) {
+    score = 0;
+  }
+
+  return score;
+}
