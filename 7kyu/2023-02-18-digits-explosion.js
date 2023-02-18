3cc60959c3e8100ddff9393363966d90026b64ca
Renato Sousa <renatonotare@gmail.com>
2023-02-18 19:01:55 -0300

    Add "Digits explosion"

---
+++ 7kyu/2023-02-18-digits-explosion.js
@@ -0,0 +1,24 @@
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+// Examples
+
+// explode("312")
+
+// should return :
+
+// "333122"
+
+// explode("102269")
+
+// should return :
+
+// "12222666666999999999"
+
+function explode(s) {
+  let result = "";
+
+  for (let i = 0; i < s.length; i++) {
+    result += s[i].repeat(s[i]);
+  }
+
+  return result;
+}
