0eba8cacba3f96f852dda7d4337730d26ac483b9
Renato Sousa <renatonotare@gmail.com>
2023-02-27 11:22:52 -0300

    Add "Simple beads count"

---
+++ 7kyu/2023-02-27-simple-beads-count.js
@@ -0,0 +1,14 @@
+// Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+// @ @@ @ @@ @ @@ @ @@ @ @@ @
+
+// Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+// If there are less than 2 blue beads return 0.
+
+function countRedBeads(n) {
+  if (n < 2) {
+    return 0;
+  } else {
+    return n * 2 - 2;
+  }
+}
