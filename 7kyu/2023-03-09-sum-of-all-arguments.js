705532d482f9414c54fe074ae018ad51b4eb7048
Renato Sousa <renatonotare@gmail.com>
2023-03-09 10:22:53 -0300

    Add "Sum of all arguments"

---
+++ 7kyu/2023-03-09-sum-of-all-arguments.js
@@ -0,0 +1,11 @@
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+function sum() {
+  return [...arguments].reduce((acc, n) => acc + n, 0);
+}
