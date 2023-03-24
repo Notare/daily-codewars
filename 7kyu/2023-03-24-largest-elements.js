e4c8afb37b9bc024a19a66539cb0b17881a22516
Renato Sousa <renatonotare@gmail.com>
2023-03-24 11:00:21 -0300

    Add "Largest Elements"

---
+++ 7kyu/2023-03-24-largest-elements.js
@@ -0,0 +1,10 @@
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// // => [6,7]
+
+function largest(n, array) {
+  return array.sort((a, b) => a - b).slice(array.length - n);
+}
