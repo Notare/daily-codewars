c11e38679eac3f17efeae833df29fa9f3df57df1
Renato Sousa <renatonotare@gmail.com>
2023-03-27 11:28:10 -0300

    Add "Area of a Circle"

---
+++ 7kyu/2023-03-27-area-of-a-circle.js
@@ -0,0 +1,15 @@
+// Complete the function circleArea so that it will return the area of a circle with the given radius. Round the returned number to two decimal places (except for Haskell). If the radius is not positive or not a number, return false.
+
+// Example:
+
+// circleArea(-1485.86);    //returns false
+// circleArea(0);           //returns false
+// circleArea(43.2673);     //returns 5881.25
+// circleArea(68);          //returns 14526.72
+// circleArea("number");    //returns false
+
+var circleArea = function (radius) {
+  return radius < 1 || isNaN(radius)
+    ? false
+    : +(Math.PI * radius ** 2).toFixed(2);
+};
