ab252d18f448d71d22f3e9034caef27ed425bca1
Renato Sousa <renatonotare@gmail.com>
2023-03-31 19:51:13 -0300

    Add "How many arguments"

---
+++ 7kyu/2023-03-31-how-many-arguments.js
@@ -0,0 +1,7 @@
+// args_count(1, 2, 3) -> 3
+// args_count(1, 2, 3, 10) -> 4
+// Create a function called args_count that returns the number of arguments provided
+
+function args_count() {
+  return arguments.length;
+}
