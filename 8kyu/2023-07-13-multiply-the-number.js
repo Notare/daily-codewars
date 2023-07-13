ee5c3d07cd9774143d621846f944a70e71a0e4b8
Renato Sousa <renatonotare@gmail.com>
2023-07-13 12:16:14 -0300

    Add "Multiply the number"

---
+++ 8kyu/2023-07-13-multiply-the-number.js
@@ -0,0 +1,11 @@
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+// multiply(3)==15 // 3 * 5¹
+// multiply(10)==250 // 10 * 5²
+// multiply(200)==25000 // 200 * 5³
+// multiply(0)==0 // 0 * 5¹
+// multiply(-3)==-15 // -3 * 5¹
+function multiply(number) {
+  const numLength = Math.abs(number).toString().split("").length;
+  return number * 5 ** numLength;
+}
