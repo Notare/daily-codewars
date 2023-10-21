934f3a73cc790a5e67b2bd663e7b1eb01f32139f
Renato Sousa <renatonotare@gmail.com>
2023-10-21 18:32:41 -0300

    Add "Power"

---
+++ 8kyu/2023-10-21-power.js
@@ -0,0 +1,20 @@
+// The goal is to create a function of two inputs number and power, that "raises" the number up to power (ie multiplies number by itself power times).
+// Examples
+
+// numberToPower(3, 2)  // -> 9 ( = 3 * 3 )
+// numberToPower(2, 3)  // -> 8 ( = 2 * 2 * 2 )
+// numberToPower(10, 6) // -> 1000000
+
+// Note: Math.pow and some other Math functions like eval() and ** are disabled.
+function numberToPower(number, power) {
+  if (power < 1) {
+    return 1;
+  }
+
+  let result = number;
+  for (let i = 1; i < power; i++) {
+    result *= number;
+  }
+
+  return result;
+}
