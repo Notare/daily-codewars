dc7f05d68c237ae2444b9daf4cfd8596929d5298
Renato Sousa <renatonotare@gmail.com>
2024-01-06 20:23:08 -0300

    Add "Who has the most money?"

---
+++ 6kyu/2024-01-06-who-has-the-most-money.js
@@ -0,0 +1,31 @@
+// You're going on a trip with some students and it's up to you to keep track of how much money each Student has. A student is defined like this:
+
+// class Student {
+//   constructor(name, fives, tens, twenties) {
+//     this.name = name;
+//     this.fives = fives;
+//     this.tens = tens;
+//     this.twenties = twenties;
+//   }
+// }
+
+// As you can tell, each Student has some fives, tens, and twenties. Your job is to return the name of the student with the most money. If every student has the same amount, then return "all".
+
+// Notes:
+
+//     Each student will have a unique name
+//     There will always be a clear winner: either one person has the most, or everyone has the same amount
+//     If there is only one student, then that student has the most money
+function sum(student) {
+  return student.fives * 5 + student.tens * 10 + student.twenties * 20;
+}
+
+function mostMoney(students) {
+  students.sort((a, b) => sum(b) - sum(a));
+
+  if (students.length > 1 && sum(students[0]) === sum(students[1])) {
+    return "all";
+  } else {
+    return students[0].name;
+  }
+}
