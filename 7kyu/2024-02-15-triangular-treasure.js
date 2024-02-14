3d64e1a5cdacba72edb2f237b7425c124cd0cb00
Renato Sousa <renatonotare@gmail.com>
2024-02-14 14:35:15 -0300

    Add "Triangular Treasure"

---
+++ 7kyu/2024-02-15-triangular-treasure.js
@@ -0,0 +1,18 @@
+// Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+// 1st (1)   2nd (3)    3rd (6)
+// *          **        ***
+//            *         **
+//                      *
+
+// You need to return the nth triangular number. You should return 0 for out of range values:
+
+// For example: (Input --> Output)
+
+// 0 --> 0
+// 2 --> 3
+// 3 --> 6
+// -10 --> 0
+function triangular(n) {
+  return n > 0 ? (n * (n + 1)) / 2 : 0;
+}
