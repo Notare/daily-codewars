16f78e25006665c30c78d40b3c77634e93c50cd2
Renato Sousa <renatonotare@gmail.com>
2024-02-18 15:29:58 -0300

    Add "My Language Skills"

---
+++ 7kyu/2024-02-18-my-language-skills.js
@@ -0,0 +1,13 @@
+// You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
+
+// Note: the scores will always be unique (so no duplicate values)
+// Examples
+
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+function myLanguages(results) {
+  return Object.keys(results)
+    .filter((lang) => results[lang] > 59)
+    .sort((a, b) => results[b] - results[a]);
+}
