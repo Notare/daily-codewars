75b096037988d622703483cf3ec834147a75531e
Renato Sousa <renatonotare@gmail.com>
2024-04-20 11:32:39 -0300

    Add "Who is the killer?"

---
+++ 7kyu/2024-04-20-who-is-the-killer.js
@@ -0,0 +1,26 @@
+// Who is the killer?
+// Some people have been killed!
+
+// You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+// Task.
+
+// Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+
+// {'James': ['Jacob', 'Bill', 'Lucas'],
+//  'Johnny': ['David', 'Kyle', 'Lucas'],
+//  'Peter': ['Lucy', 'Kyle']}
+
+// and also a list of the names of the dead people:
+
+// ['Lucas', 'Bill']
+
+// return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill'
+function killer(suspectInfo, dead) {
+  for (const suspect in suspectInfo) {
+    const murderedArr = suspectInfo[suspect];
+
+    if (dead.every((deadPerson) => murderedArr.includes(deadPerson))) {
+      return suspect;
+    }
+  }
+}
