21f7750788abf3970b95a9141f2e2922c62e95bd
Renato Sousa <renatonotare@gmail.com>
2024-05-12 11:05:35 -0300

    Add "The old switcheroo"

---
+++ 7kyu/2024-05-12-the-old-switcheroo.js
@@ -0,0 +1,15 @@
+// Write a function
+
+// vowel2index(str)
+
+// that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+// E.g:
+
+// vowel2index('this is my string') == 'th3s 6s my str15ng'
+// vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+// vowel2index('') == ''
+
+// Your function should be case insensitive to the vowels.
+function vowel2index(str) {
+  return str.replace(/[aeiou]/gi, (_, i) => i + 1);
+}
