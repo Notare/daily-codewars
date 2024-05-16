fbf44ded2fa111d5be3f605f280b76368375edc0
Renato Sousa <renatonotare@gmail.com>
2024-05-16 12:45:51 -0300

    Add "Regexp Basics - is it a letter?"

---
+++ 7kyu/2024-05-16-regexp-basics---is-it-a-letter.js
@@ -0,0 +1,4 @@
+// Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
+String.prototype.isLetter = function () {
+  return /^[a-z]$/i.test(this);
+};
