16d705be587dc223d84466241c2b4d99909bdc52
Renato Sousa <renatonotare@gmail.com>
2024-06-19 15:15:47 -0300

    Add "Count strings in objects"

---
+++ 7kyu/2024-06-19-count-strings-in-objects.js
@@ -0,0 +1,25 @@
+// Create a function strCount (takes an object as argument) that will count all string values inside an object. For example:
+
+// strCount({
+//   first: "1",
+//   second: "2",
+//   third: false,
+//   fourth: ["anytime",2,3,4],
+//   fifth:  null
+//   })
+//   //returns 3
+function strCount(obj) {
+  let count = 0;
+
+  for (const key in obj) {
+    if (typeof obj[key] === "object") {
+      count += strCount(obj[key]);
+    }
+
+    if (typeof obj[key] === "string") {
+      count++;
+    }
+  }
+
+  return count;
+}
