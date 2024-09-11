c01e39f27693934cbfdb8ab2c4f47695f208119d
Renato Sousa <renatonotare@gmail.com>
2024-09-10 21:11:43 -0300

    Add "Simple string reversal"

---
+++ 7kyu/2024-09-10-simple-string-reversal.js
@@ -0,0 +1,19 @@
+// In this Kata, we are going to reverse a string while maintaining the spaces (if any) in their original place.
+
+// For example:
+
+// solve("our code") = "edo cruo"
+// -- Normal reversal without spaces is "edocruo".
+// -- However, there is a space at index 3, so the string becomes "edo cruo"
+
+// solve("your code rocks") = "skco redo cruoy".
+// solve("codewars") = "srawedoc"
+
+// More examples in the test cases. All input will be lower case letters and in some cases spaces.
+function solve(str) {
+  const chars = str.split("");
+  const charsReversed = chars.filter((letter) => letter !== " ").reverse();
+  return chars
+    .map((char) => (char === " " ? " " : charsReversed.shift()))
+    .join("");
+}
